7aa24126e999bd14c2c8da99f6ea4dda7aa9a6e2
Tom Klaver <tomklav@gmail.com>
2020-02-24 16:06:24 +0100

    set clang & wasm-ld versions in build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-clang --target=wasm32 -O3 -nostdlib -Wl,--no-entry -Wl,--export-all -o base64.wasm base64.c
+#clang --target=wasm32 -O3 -nostdlib -Wl,--no-entry -Wl,--export-all -o base64.wasm base64.c
+clang-11 -fuse-ld=/usr/bin/wasm-ld-11 --target=wasm32 -O3 -nostdlib -Wl,--no-entry -Wl,--export-all -o base64.wasm base64_encode.c
